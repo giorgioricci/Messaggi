984e4ddd16bcdb91c44722b90ca7841ffc98c7ea
Giorgio <gio.ricci@gmail.com>
2022-03-08 12:34:29 +0100

    Aggiorna pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,41 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    if (setMessaggi == 1) {
-        radio.sendString("" + (facciaFelice))
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        basic.pause(1000)
-        basic.clearScreen()
-    } else if (setPassi == 1) {
-        basic.showString("" + (valorePassi))
-        basic.pause(5000)
-        basic.clearScreen()
-    }
-})
-input.onGesture(Gesture.Shake, function () {
-    if (setPassi == 1) {
-        valorePassi += 1
-        basic.showString("" + (valorePassi))
-        basic.clearScreen()
-    }
+    radio.sendString("" + (facciaFelice))
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
-    if (setMessaggi == 1) {
-        radio.sendString("facciaCacca")
-        basic.showLeds(`
-            . . . . .
-            . . . # .
-            . . # . .
-            . # # # .
-            # # # # #
-            `)
-        basic.pause(1000)
-        basic.clearScreen()
-    }
+    radio.sendString("" + (facciaCacca))
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == facciaFelice) {
@@ -98,43 +83,22 @@ radio.onReceivedString(function (receivedString) {
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (setMessaggi == 1) {
-        radio.sendString("" + (facciaTriste))
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # # # .
-            # . . . #
-            `)
-        basic.pause(1000)
-        basic.clearScreen()
-    } else if (setPassi == 1) {
-        valorePassi = 0
-        basic.showString("" + (valorePassi))
-    }
+    radio.sendString("" + (facciaTriste))
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
 })
-let valorePassi = 0
-let setPassi = 0
-let setMessaggi = 0
 let facciaCacca = ""
 let facciaTriste = ""
 let facciaFelice = ""
 radio.setGroup(5)
-music.setVolume(127)
+music.setVolume(0)
 facciaFelice = "facciaFelice"
 facciaTriste = "facciaTriste"
 facciaCacca = "facciaCacca"
-basic.showString("A o B?")
-basic.pause(2000)
-if (input.buttonIsPressed(Button.A)) {
-    setMessaggi = 1
-    basic.showString("Facce")
-} else if (input.buttonIsPressed(Button.B)) {
-    setPassi = 1
-    valorePassi = 0
-    basic.showString("Passi")
-} else {
-    basic.showString("Non scelto")
-}
-basic.pause(1000)
